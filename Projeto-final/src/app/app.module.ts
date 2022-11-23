def032f091f8d0529a35b7bbf7b77f42eba4c122
Rafael Simionato <rafaelsimionato64@gmail.com>
2022-11-23 18:43:55 -0300

    Html e css de página "Locadora" feito

--- Projeto-final/src/app/app.module.ts
+++ Projeto-final/src/app/app.module.ts
@@ -3,19 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FooterComponent } from './components/footer/footer.component';
+import { LocadoraComponent } from './components/locadora/locadora.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
+    LocadoraComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
