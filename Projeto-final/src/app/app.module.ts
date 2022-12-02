3ebd0a9a35a768f93095af8b6953b8d72fdf6c09
cadu0007 <carlos.duduilha1@gmail.com>
2022-12-02 01:29:20 -0300

    cadastro finalizado

--- Projeto-final/src/app/app.module.ts
+++ Projeto-final/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import { HttpClientModule } from '@angular/common/http'
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +17,12 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+   MatCardModule,
+   MatInputModule,
+   HttpClientModule,
+   ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Projeto-final/src/app/cadastrar/cadastrar.component.ts
+++ Projeto-final/src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UsuarioService } from '../usuario.service'
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +9,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  listaUsuarios: any;
+
+  form!: FormGroup;
+
+  constructor(private serviceUsuario: UsuarioService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.form = this.fb.group({
+      nome: '',
+      email: '',
+      telefone: '',
+      senha: ''
+    })
+
+    this.listarUsuario()
   }
 
+  listarUsuario() {
+    this.serviceUsuario.getUsuarios().subscribe({
+      next: (d: any) => {
+        this.listaUsuarios = d;
+        console.log(this.listaUsuarios);
+
+      },
+      error: (e: any) => {
+        console.log("algo deu errado" + e);
+      }
+    })
+  }
+
+  cadastrarDadosUsuarios() {
+
+    let nomeInput = this.form.controls["nome"].value
+    let emailInput = this.form.controls["email"].value
+    let telefoneInput = this.form.controls["telefone"].value
+    let senhaInput =  this.form.controls["senha"].value
+
+    let dados = {
+      nome: nomeInput,
+      email: emailInput,
+      telefone: telefoneInput,
+      senha: senhaInput
+    }
+    
+    this.serviceUsuario.postUsuarios(dados).subscribe({
+      next: (d: any) => {
+        console.log(d);
+        this.listarUsuario()
+      },
+      error: (e: any) => {
+        console.log("algo deu errado" + e);
+      }
+    })
+  }
 }
